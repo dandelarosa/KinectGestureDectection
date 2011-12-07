8e8dd3031801cdaeb125923ad81edeba3c80a2be
George Poulos <poulos@gmail.com>
2011-12-07 12:16:51 -0500

    Changed ArrowCursorTarget to be a more specific ShapeCursorTarget.

--- KinectGestureDectection/ArrowCursorTarget.cs
+++ KinectGestureDectection/ArrowCursorTarget.cs
@@ -9,84 +9,15 @@ using System.Windows.Shapes;
 
 namespace KinectGestureDectection
 {
-    class ArrowCursorTarget : CursorTarget
+    class ArrowCursorTarget : ShapeCursorTarget
     {
-        private readonly Brush nonSelectedFillColor = Brushes.LightBlue;
-        private readonly Brush selectedColor = Brushes.OrangeRed;
-        private readonly Brush nonSelectedStrokeColor = Brushes.WhiteSmoke;
-        private readonly Canvas canvas;
-        private Shape shape;
-
-        public ArrowCursorTarget(Canvas canvas, Point center, int radius) : base(center, radius)
-        {
-            this.canvas = canvas;
-        }
-
-        public Transform LayoutTransform
-        {
-            get { return shape.LayoutTransform; }
-            set { shape.LayoutTransform = value; }
-        }
-
-        public override bool IsEnabled
-        {
-            get { return base.IsEnabled; }
-            set
-            {
-                base.IsEnabled = value;
-                if (canvas == null) 
-                    return;
-                bool containsShape = canvas.Children.Contains(shape);
-                if (value)
-                {
-                    if (!containsShape)
-                        canvas.Children.Add(shape);
-                }
-                else
-                {
-                    if (containsShape)
-                        canvas.Children.Remove(shape);
-                }
-            }
-        }
-
-        protected override void SetBounds(Point center, int radius)
-        {
-            base.SetBounds(center, radius);
-            if (shape == null)
-            {
-                shape = new Arrow
-                {
-                    HorizontalAlignment = HorizontalAlignment.Left,
-                    VerticalAlignment = VerticalAlignment.Top,
-                    StrokeThickness = 1.5,
-                    Stroke = nonSelectedStrokeColor,
-                    Fill = nonSelectedFillColor,
-                };
-            }
-            shape.Width = Bounds.Width;
-            shape.Height = Bounds.Height;
-            Canvas.SetLeft(shape, Bounds.Left);
-            Canvas.SetTop(shape, Bounds.Top);
-        }
-
-        protected override void onCursorEnter()
-        {
-            shape.Stroke = selectedColor;
-            base.onCursorEnter();
-        }
-
-        protected override void onCursorSelect()
-        {
-            shape.Fill = selectedColor;
-            base.onCursorSelect();
-        }
-
-        protected override void onCursorLeave()
-        {
-            shape.Fill = nonSelectedFillColor;
-            shape.Stroke = nonSelectedStrokeColor;
-            base.onCursorLeave();
-        }
+        public ArrowCursorTarget(Canvas canvas, Point center, int radius) :
+            base(new Arrow
+             {
+                 HorizontalAlignment = HorizontalAlignment.Left,
+                 VerticalAlignment = VerticalAlignment.Top,
+                 StrokeThickness = 1.5,
+             }, canvas, center, radius)
+        { }
     }
 }
---
+++ KinectGestureDectection/ShapeCursorTarget.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Media;
+using System.Windows.Shapes;
+using System.Windows.Controls;
+using System.Windows;
+
+namespace KinectGestureDectection
+{
+    class ShapeCursorTarget : CursorTarget
+    {
+        private readonly Canvas canvas;
+
+        public Brush NonSelectedFill { get; set; }
+        public Brush SelectedFill { get; set; }
+        public Brush NonSelectedStroke { get; set; }
+        public Brush SelectedStroke { get; set; }
+
+        public Shape Shape { get; private set; }
+
+        public ShapeCursorTarget(Shape shape, Canvas canvas, Point center, int radius)
+            :this(shape, canvas, center, radius, radius){}
+
+        public ShapeCursorTarget(Shape shape, Canvas canvas, Point center, int width, int height)
+        {
+            this.canvas = canvas;
+            this.Shape = shape;
+            this.Shape.Fill = this.NonSelectedFill = Brushes.LightBlue;
+            this.Shape.Stroke = this.NonSelectedStroke = Brushes.WhiteSmoke;
+            this.SelectedFill = Brushes.OrangeRed;
+            this.NonSelectedFill = Brushes.OrangeRed;
+            SetBounds(center, width, height);
+        }
+
+        public override bool IsEnabled
+        {
+            get { return base.IsEnabled; }
+            set
+            {
+                base.IsEnabled = value;
+                if (canvas == null)
+                    return;
+                bool containsShape = canvas.Children.Contains(Shape);
+                if (value)
+                {
+                    if (!containsShape)
+                        canvas.Children.Add(Shape);
+                }
+                else
+                {
+                    if (containsShape)
+                        canvas.Children.Remove(Shape);
+                }
+            }
+        }
+
+        protected override void SetBounds(Point center, int width, int height)
+        {
+            base.SetBounds(center, width, height);
+            Shape.Width = Bounds.Width;
+            Shape.Height = Bounds.Height;
+            Canvas.SetLeft(Shape, Bounds.Left);
+            Canvas.SetTop(Shape, Bounds.Top);
+        }
+
+        protected override void onCursorEnter()
+        {
+            Shape.Stroke = SelectedStroke;
+            base.onCursorEnter();
+        }
+
+        protected override void onCursorSelect()
+        {
+            Shape.Fill = SelectedFill;
+            base.onCursorSelect();
+        }
+
+        protected override void onCursorLeave()
+        {
+            Shape.Fill = NonSelectedFill;
+            Shape.Stroke = NonSelectedStroke;
+            base.onCursorLeave();
+        }
+    }
+}
