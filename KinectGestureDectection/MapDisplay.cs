fbd7eb7b0a0005572140918fb9447eabc5d0a780
Dan Dela Rosa <dandelarosa@gmail.com>
2011-12-14 14:37:07 -0500

    Changed appearance of the map

--- KinectGestureDectection/MapDisplay.cs
+++ KinectGestureDectection/MapDisplay.cs
@@ -21,29 +21,47 @@ namespace KinectGestureDectection
             this.canvas = canvas;
             this.game = game;
 
+            // TODO don't know if this should be hardcoded or calculated by some other means
+            int panelWidth = 300;
+            int panelHeight = 300;
+
+            // Calculate map position
+            int panelLeftX = ((int)canvas.Width - panelWidth) >> 1;
+            int panelTopY = ((int)canvas.Height - panelHeight) >> 1;
+
             mapBG = new Rectangle();
-            mapBG.Height = 100;
-            mapBG.Width = 100;
-            Canvas.SetTop(mapBG, 100);
-            Canvas.SetLeft(mapBG, 100);
+            mapBG.Height = panelHeight;
+            mapBG.Width = panelWidth;
+            Canvas.SetTop(mapBG, panelTopY);
+            Canvas.SetLeft(mapBG, panelLeftX);
             mapBG.Fill = new SolidColorBrush(Color.FromArgb(255, 255, 255, 255));
-            
+
+            int titleHeight = 50;
             mapTitle = new TextBlock();
             mapTitle.Text = "Map";
-            Canvas.SetTop(mapTitle, 100);
-            Canvas.SetLeft(mapTitle, 100);
+            mapTitle.FontSize = 28;
+            mapTitle.Width = panelWidth;
+            mapTitle.Height = titleHeight;
+            Canvas.SetTop(mapTitle, panelTopY);
+            Canvas.SetLeft(mapTitle, panelLeftX);
+            mapTitle.HorizontalAlignment = System.Windows.HorizontalAlignment.Center;
             
             rooms = new Rectangle[Game.mapWidth, Game.mapHeight];
+            int gapWidth = 20;
+            int cellWidth = (panelWidth - (gapWidth * (Game.mapWidth + 1))) / Game.mapWidth;
+            int gapHeight = 20;
+            int cellHeight = (panelHeight - (gapHeight * (Game.mapHeight + 1)) - titleHeight) / Game.mapHeight;
 
             for (int i = 0; i < Game.mapWidth; i++)
             {
                 for (int j = 0; j < Game.mapHeight; j++)
                 {
                     rooms[i, j] = new Rectangle();
-                    rooms[i, j].Height = 10;
-                    rooms[i, j].Width = 10;
-                    Canvas.SetTop(rooms[i, j], 200 - 20 * j - 20); // y = 0 is map bottom
-                    Canvas.SetLeft(rooms[i, j], 100 + 20 * i + 20);
+                    rooms[i, j].Height = cellHeight;
+                    rooms[i, j].Width = cellWidth;
+                    // y = 0 is map bottom
+                    Canvas.SetTop(rooms[i, j], panelTopY + panelHeight - j * (cellHeight + gapHeight) - gapHeight - cellHeight);
+                    Canvas.SetLeft(rooms[i, j], panelLeftX + i * (cellWidth + gapWidth) + gapWidth);
                 }
             }
         }
@@ -58,11 +76,15 @@ namespace KinectGestureDectection
                 {
                     if (i == game.mapPositionX && j == game.mapPositionY)
                     {
-                        rooms[i, j].Fill = new SolidColorBrush(Color.FromArgb(255, 128, 128, 128));
+                        rooms[i, j].Fill = new SolidColorBrush(Color.FromArgb(255, 0, 128, 0));
+                    }
+                    else if (game.gameMap[i, j].currentEnemyLife > 0)
+                    {
+                        rooms[i, j].Fill = new SolidColorBrush(Color.FromArgb(255, 128, 0, 0));
                     }
                     else
                     {
-                        rooms[i, j].Fill = new SolidColorBrush(Color.FromArgb(255, 0, 0, 0));
+                        rooms[i, j].Fill = new SolidColorBrush(Color.FromArgb(255, 128, 128, 128));
                     }
                     canvas.Children.Add(rooms[i, j]);
                 }
